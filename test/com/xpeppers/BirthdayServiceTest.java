8b3a0194cb0feb3c770333cd02adaf7d09665a91
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-09-01 14:35:29 +0200

    Updated .gitignore

---
+++ test/com/xpeppers/BirthdayServiceTest.java
@@ -0,0 +1,11 @@
+package com.xpeppers;
+
+import org.junit.Test;
+
+public class BirthdayServiceTest {
+
+    @Test
+    public void testWhenSendGreetingIsCalledEmployeesRepositoryCallsGetEmployees() throws Exception {
+//        BirthdayService birthdayService = new BirthdayService();
+    }
+}
